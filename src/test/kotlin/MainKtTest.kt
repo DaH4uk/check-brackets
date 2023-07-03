16e6d620ce658b47a3378d016f2f97db5e80b7bf
Turov Danil <turovdanil@mail.ru>
2023-07-03 13:58:36 +0500

    fix test imports

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -1,36 +1,37 @@
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
 class MainKtTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun `should return false for random string`() {
         val testCase = "Hello, world!"
 
         assertFalse(checkBrackets(testCase, openingToClosingBrackets))
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun `should return true for valid string`() {
         val testCase = "((({{}})))()[][][{{}}]"
 
         assertTrue(checkBrackets(testCase, openingToClosingBrackets))
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun `should return false for invalid string`() {
         val testCase = "({)}]"
 
         assertFalse(checkBrackets(testCase, openingToClosingBrackets))
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun `should return false for string with unpaired opening brackets`() {
         val testCase = "({{[("
 
         assertFalse(checkBrackets(testCase, openingToClosingBrackets))
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun `should return false for string with unpaired closing brackets`() {
         val testCase = "}}])"
 
