be611419bcf25da2a6ab722c5a8aa5795ba003fa
Turov Danil <turovdanil@mail.ru>
2023-07-03 13:56:32 +0500

    rename the project

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,38 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.Stack
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+// moved the map from the function to make it more expandable
+val openingToClosingBrackets = mapOf('{' to '}', '[' to ']', '(' to ')')
+
+fun checkBrackets(inputString: String, openingToClosingBrackets: Map<Char, Char>): Boolean {
+    val openingBrackets = openingToClosingBrackets.keys
+    val closingBrackets = openingToClosingBrackets.values
+
+    val stack = Stack<Char>()
+
+    for (char in inputString) {
+        // optional checking if char is not a bracket at all
+        if (!openingBrackets.contains(char) && !closingBrackets.contains(char)) {
+            return false
+        }
+
+        // add opening bracket to stack
+        if (openingBrackets.contains(char)) {
+            stack.push(char)
+
+        // check if closing bracket matches last opening bracket
+        } else if (closingBrackets.contains(char)) {
+            // return false if stack is empty
+            if (stack.isEmpty()) {
+                return false
+            }
+            val last = stack.pop()
+            // return false if closing bracket doesn't match last opening bracket
+            if (openingToClosingBrackets[last] != char) {
+                return false
+            }
+        }
+    }
+
+    // return false if stack is not empty
+    return stack.isEmpty()
 }
--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -3,6 +3,37 @@ import org.junit.jupiter.api.Assertions.*
 class MainKtTest {
 
     @org.junit.jupiter.api.Test
-    fun isValid() {
+    fun `should return false for random string`() {
+        val testCase = "Hello, world!"
+
+        assertFalse(checkBrackets(testCase, openingToClosingBrackets))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun `should return true for valid string`() {
+        val testCase = "((({{}})))()[][][{{}}]"
+
+        assertTrue(checkBrackets(testCase, openingToClosingBrackets))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun `should return false for invalid string`() {
+        val testCase = "({)}]"
+
+        assertFalse(checkBrackets(testCase, openingToClosingBrackets))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun `should return false for string with unpaired opening brackets`() {
+        val testCase = "({{[("
+
+        assertFalse(checkBrackets(testCase, openingToClosingBrackets))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun `should return false for string with unpaired closing brackets`() {
+        val testCase = "}}])"
+
+        assertFalse(checkBrackets(testCase, openingToClosingBrackets))
     }
 }
